82614f530f25186711bbd8758eec9b44a2a97bd9
jarviszjyao <100833573+jarviszjyao@users.noreply.github.com>
2025-07-15 21:25:44 +0800

    changed to use efs

--- lambda_scripts/ecs_task_discovery/ecs_eventbridge.tf
+++ lambda_scripts/ecs_task_discovery/ecs_eventbridge.tf
@@ -13,15 +13,37 @@ variable "lambda_function_arn" {
   type        = string
 }
 
-variable "eventbridge_rule_name" {
-  description = "EventBridge规则名称"
+variable "eventbridge_rule_name_task" {
+  description = "EventBridge规则名称（Task State Change）"
   type        = string
-  default     = "ecs-service-autoscaling-to-lambda"
+  default     = "ecs-task-state-change-to-lambda"
 }
 
-resource "aws_cloudwatch_event_rule" "ecs_service_autoscaling" {
-  name        = var.eventbridge_rule_name
-  description = "Trigger Lambda on ECS Service Auto Scaling events"
+variable "eventbridge_rule_name_scale" {
+  description = "EventBridge规则名称（Service Action）"
+  type        = string
+  default     = "ecs-service-action-to-lambda"
+}
+
+# ECS Task State Change
+resource "aws_cloudwatch_event_rule" "ecs_task_state_change" {
+  name        = var.eventbridge_rule_name_task
+  description = "Trigger Lambda on ECS Task State Change"
+  event_pattern = jsonencode({
+    "source": ["aws.ecs"],
+    "detail-type": ["ECS Task State Change"],
+    "detail": {
+      "clusterArn": [var.ecs_cluster_arn],
+      "group": ["service:${var.ecs_service_name}"],
+      "lastStatus": ["RUNNING", "STOPPED"]
+    }
+  })
+}
+
+# ECS Service Action (Auto Scaling)
+resource "aws_cloudwatch_event_rule" "ecs_service_action" {
+  name        = var.eventbridge_rule_name_scale
+  description = "Trigger Lambda on ECS Service Action"
   event_pattern = jsonencode({
     "source": ["aws.application-autoscaling"],
     "detail-type": ["ECS Service Action"],
@@ -34,15 +56,48 @@ resource "aws_cloudwatch_event_rule" "ecs_service_autoscaling" {
   })
 }
 
-resource "aws_cloudwatch_event_target" "lambda" {
-  rule      = aws_cloudwatch_event_rule.ecs_service_autoscaling.name
+resource "aws_cloudwatch_event_target" "lambda_task" {
+  rule      = aws_cloudwatch_event_rule.ecs_task_state_change.name
   arn       = var.lambda_function_arn
 }
 
-resource "aws_lambda_permission" "allow_eventbridge" {
-  statement_id  = "AllowExecutionFromEventBridge"
+resource "aws_cloudwatch_event_target" "lambda_scale" {
+  rule      = aws_cloudwatch_event_rule.ecs_service_action.name
+  arn       = var.lambda_function_arn
+}
+
+resource "aws_lambda_permission" "allow_eventbridge_task" {
+  statement_id  = "AllowExecutionFromEventBridgeTask"
   action        = "lambda:InvokeFunction"
   function_name = var.lambda_function_arn
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.ecs_service_autoscaling.arn
+  source_arn    = aws_cloudwatch_event_rule.ecs_task_state_change.arn
+}
+
+resource "aws_lambda_permission" "allow_eventbridge_scale" {
+  statement_id  = "AllowExecutionFromEventBridgeScale"
+  action        = "lambda:InvokeFunction"
+  function_name = var.lambda_function_arn
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.ecs_service_action.arn
+}
+
+# Lambda需要有ssm:SendCommand权限
+resource "aws_iam_policy" "lambda_ssm_send_command" {
+  name        = "lambda-ssm-send-command"
+  description = "Allow Lambda to send SSM commands"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ssm:SendCommand",
+          "ssm:ListDocuments",
+          "ssm:ListCommandInvocations"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
 } 
